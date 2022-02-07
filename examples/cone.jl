dc0734b80971773dea47fba820b5bfb5b5672c6e
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2022-02-07 14:47:35 +0530

    cone and shape updated for create sig

--- examples/cone.jl
+++ examples/cone.jl
@@ -3,6 +3,6 @@ Pkg.activate("./")
 
 using PDMesh
 
-x, v, y, vol = create(Cone(10.0, 20.0), resolution=0.5, rand_=0.0)
+x, v, y, vol, type_ = unpack(create(Cone(10.0, 20.0), resolution=0.5, rand_=0.0)
 
 write_data("./examples/ovito.data", x, vol, ones(Int64, size(x)[2]))
